af46e62059c2efb50e44abcf31113bd867ca586c
Park-minseong <qkralstjd02@gmail.com>
2022-10-19 22:49:02 +0900

    mysql connection후 연동하여 api생성

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs"); //파일에 접근 할 수 있는 라이브러리 - mysql연결 시 사용
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,10 +7,22 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json"); // fs를 이용하여 mysql 설정파일 불러옴
+const conf = JSON.parse(data); //json형태로 파싱
+const mysql = require("mysql"); //mysql라이브러이 로딩
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+}); //createConnection 연결 수행, connection 변수는 database.json의 내용을 커넥션에 적용하여 연결한 객체 초기화
+connection.connect(); // db연결
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    { name: "park", tel: "010-4113-8018", zip: "서울특별시" },
-    { name: "lee", tel: "010-9695-9115", zip: "서울특별시" },
-  ]);
+  connection.query("SELECT * FROM T_USER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
